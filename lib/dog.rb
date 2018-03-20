1922ed1ed6a4a9e5be4f12982b578f2bf8ce4acd
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-20 22:00:12 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -78,8 +78,8 @@ attr_accessor :name, :breed, :id
     new_song = self.new
     new_song.id = row[0]
     new_song.name =  row[1]
-    new_song.length = row[2]
-    new_song  
+    new_song.breed = row[2]
+    new_song
   end
 
 

ff874bef979f8851a3cfc0e1cc57405ed0a3ecca
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-20 20:43:24 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,5 +8,17 @@ attr_accessor :name, :breed, :id
     @id = id
   end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE dogs(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT,
+      )
+    SQL
+
+    DB[:conn].execute(sql)  
+  end
+
 
 end
